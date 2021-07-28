e8d2b566a41a63c593bdade1732154b9191cb160
alan-pc <alanluc9@gmail.com>
2021-07-28 11:05:52 -0400

    fixed proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware")
 
 module.exports = (app) => {
     app.use(
-        createProxyMiddleware("/", {
+        createProxyMiddleware("/profile", {
             target: "http://localhost:8000",
             changeOrigin: true
         })
